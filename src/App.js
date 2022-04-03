1c696f0582234fd48f648d679353bba943066596
xoxo <thakursatyam48@gmail.com>
2022-04-03 11:22:17 +0530

    added navbar and page components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is homepage
-      </Route>
+    <div>
+      <Navs />
 
-      <Route exact path="/starred">
-        This is starred
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route>404 Page Not Found</Route>
-    </Switch>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+
+        <Route>
+          <div>404 Not Found</div>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
