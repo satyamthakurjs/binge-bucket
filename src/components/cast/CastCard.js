71bc347919b4a8d79291f5a30aa5688a26c1355a
xoxo <thakursatyam48@gmail.com>
2022-04-03 21:09:31 +0530

    integration of styled components and styles for cards

--- src/components/cast/CastCard.js
+++ src/components/cast/CastCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { StyledCastCard } from './CastCard.styled';
 
 const CastCard = ({ image, name, gender, country, birthday, deathday }) => {
   return (
-    <div>
-      <div>
+    <StyledCastCard>
+      <div className="img-wrapper">
         <img src={image} alt="cast" />
       </div>
       <h1>
@@ -11,8 +12,8 @@ const CastCard = ({ image, name, gender, country, birthday, deathday }) => {
       </h1>
       <p>{country ? `From ${country}` : null}</p>
       {birthday ? <p>Born {birthday}</p> : null}
-      <p>{deathday ? `Died ${deathday}` : null}</p>
-    </div>
+      <p className="deathday">{deathday ? `Died ${deathday}` : null}</p>
+    </StyledCastCard>
   );
 };
 
--- src/components/cast/CastGrid.js
+++ src/components/cast/CastGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import CastCard from './CastCard';
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
+import { FlexGrid } from '../styled';
 
 const CastGrid = ({ data }) => {
   return (
-    <div>
+    <FlexGrid>
       {data.map(({ person }) => (
         <CastCard
           key={person.id}
@@ -16,7 +17,7 @@ const CastGrid = ({ data }) => {
           image={person.image ? person.image.medium : IMAGE_NOT_FOUND}
         />
       ))}
-    </div>
+    </FlexGrid>
   );
 };
 
